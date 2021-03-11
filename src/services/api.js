69771bef064be2c54e283510e0920a6ce4e8cdde
JeremyGillard <gillard.jeremy@outlook.fr>
2021-03-11 16:08:46 +0100

    feat(api): fake data api

---
+++ src/services/api.js
@@ -0,0 +1,52 @@
+export function getTerms() {
+  return [
+    {
+      id: 0,
+      term: 'hello',
+      translation: 'bonjour',
+      reviewed: true,
+      numberCorrectAnswer: 1,
+      numberWrongAnswer: 0,
+    },
+    {
+      id: 1,
+      term: 'hi',
+      translation: 'salut',
+      reviewed: true,
+      numberCorrectAnswer: 0,
+      numberWrongAnswer: 1,
+    },
+    {
+      id: 2,
+      term: 'where',
+      translation: 'où',
+      reviewed: false,
+      numberCorrectAnswer: 0,
+      numberWrongAnswer: 0,
+    },
+    {
+      id: 3,
+      term: 'when',
+      translation: 'quand',
+      reviewed: false,
+      numberCorrectAnswer: 0,
+      numberWrongAnswer: 0,
+    },
+    {
+      id: 4,
+      term: 'why',
+      translation: 'pourquoi',
+      reviewed: false,
+      numberCorrectAnswer: 0,
+      numberWrongAnswer: 0,
+    },
+    {
+      id: 3,
+      term: 'what',
+      translation: 'quoi',
+      reviewed: false,
+      numberCorrectAnswer: 0,
+      numberWrongAnswer: 0,
+    },
+  ];
+}
