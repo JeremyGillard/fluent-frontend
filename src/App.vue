53d59ea93cdfb4c6413c415888cfe625c7b729a9
JeremyGillard <gillard.jeremy@outlook.fr>
2021-03-11 18:11:26 +0100

    feat(flashcard): FlashCard creation

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,25 @@
       :reviewingLength="reviewing.length"
       :masteredLength="mastered.length"
     />
+    <flash-card
+      :term="currentTerm.term"
+      :translation="currentTerm.translation"
+      :reviewed="currentTerm.reviewed"
+      :color="flashCardColor"
+    />
   </main>
 </template>
 
 <script>
 import ListIndicators from './components/ListIndicators.vue';
+import FlashCard from './components/FlashCard.vue';
 import { getTerms } from './services/api';
 
 export default {
   name: 'App',
   components: {
     ListIndicators,
+    FlashCard,
   },
   data() {
     return {
@@ -36,6 +44,24 @@ export default {
     },
     mastered() {
       return this.terms.filter((term) => !!term.numberCorrectAnswer);
+    },
+    currentTerm() {
+      if (this.learning) {
+        return this.learning[0];
+      } else if (this.reviewing) {
+        return this.reviewing[0];
+      } else {
+        return this.mastered[0];
+      }
+    },
+    flashCardColor() {
+      if (this.learning) {
+        return '#34B6FF';
+      } else if (this.reviewing) {
+        return '#ffc634';
+      } else {
+        return '#4A4A4A';
+      }
     },
   },
 };
---
+++ src/components/FlashCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <section @click="handleToggle" class="flash-card">
+    <p class="content">{{ content }}</p>
+    <div class="light">
+      <div class="icon"><i :class="toggledClassName"></i></div>
+      <div class="tablet" :style="{ backgroundColor: color }"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    term: {
+      type: String,
+      required: true,
+    },
+    translation: {
+      type: String,
+      required: true,
+    },
+    reviewed: {
+      type: Boolean,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      toggle: false,
+      className: '',
+    };
+  },
+  computed: {
+    content() {
+      return this.toggle ? this.translation : this.term;
+    },
+    toggledClassName() {
+      return this.toggle ? 'fas fa-eye-slash' : 'fas fa-eye';
+    },
+  },
+  methods: {
+    handleToggle() {
+      this.toggle = !this.toggle;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.flash-card {
+  width: 16rem;
+  box-shadow: 0px 4px 16px 1px rgba(0, 0, 0, 0.2);
+  border-radius: 0.5rem;
+  margin: 1.5rem auto 1rem;
+  padding: 1rem;
+}
+.content {
+  font-size: 1.375rem;
+  color: '#4a4a4a';
+  margin: 0 auto 1.375rem;
+  text-align: center;
+}
+.light {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+}
+.icon i {
+  font-size: 1.5rem;
+  color: '#4a4a4a';
+}
+.tablet {
+  width: 0.875rem;
+  height: 0.875rem;
+  border-radius: 0.5rem;
+  margin-left: 1rem;
+}
+</style>
