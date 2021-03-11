899545d626bed015fa39dba1ba41c0b9c4dcbd2b
JeremyGillard <gillard.jeremy@outlook.fr>
2021-03-11 16:15:03 +0100

    feat(app): computed, split data into three parts

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <h1>My App!</h1>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { getTerms } from './services/api';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      terms: [],
+    };
+  },
+  created() {
+    this.terms = getTerms();
+  },
+  computed: {
+    learning() {
+      return this.terms.filter((term) => !term.reviewed);
+    },
+    reviewing() {
+      return this.terms.filter(
+        ({ reviewed, numberCorrectAnswer }) => reviewed && !numberCorrectAnswer,
+      );
+    },
+    mastered() {
+      return this.terms.filter((term) => !!term.numberCorrectAnswer);
+    },
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
