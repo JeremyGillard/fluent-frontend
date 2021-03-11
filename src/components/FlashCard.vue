38dff53d4f2b208aa7ea020035d3577f60549af1
JeremyGillard <gillard.jeremy@outlook.fr>
2021-03-11 20:55:08 +0100

    feat(flashcard): flashcard disable text selection

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -36,7 +36,7 @@ export default {
 
 <style lang="scss" scoped>
 .flash-card {
-  width: 16rem;
+  width: 16rem; /* 68% if prefered */
   box-shadow: 0px 4px 16px 1px rgba(0, 0, 0, 0.2);
   border-radius: 0.5rem;
   margin: 1.5rem auto 1rem;
@@ -47,6 +47,7 @@ export default {
   color: '#4a4a4a';
   margin: 0 auto 1.375rem;
   text-align: center;
+  user-select: none;
 }
 .light {
   display: flex;
