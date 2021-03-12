98517625caa078af6d5347d006616e4ce1f80fab
JeremyGillard <gillard.jeremy@outlook.fr>
2021-03-12 10:40:59 +0100

    feat(home): pie chart v1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="home">
-    <svg class="d3-space"></svg>
     <header>
+      <svg class="chart" id="chart"></svg>
       <p class="title">Total</p>
       <p class="number">{{ total }}</p>
     </header>
@@ -20,6 +20,7 @@
 
 <script>
 import ListIndicators from '@/components/ListIndicators.vue';
+import * as d3 from 'd3';
 
 export default {
   components: {
@@ -37,6 +38,51 @@ export default {
       return this.learningLength + this.reviewingLength + this.masteredLength;
     },
   },
+  mounted() {
+    this.$nextTick(this.generateChart());
+  },
+  methods: {
+    generateChart() {
+      const data = [
+        this.learningLength,
+        this.reviewingLength,
+        this.masteredLength,
+      ];
+
+      const color = d3
+        .scaleOrdinal()
+        .domain(data)
+        .range(['#34B6FF', '#ffc634', '#4A4A4A']);
+
+      const width = 243;
+      const height = width;
+
+      const svg = d3
+        .select('#chart')
+        .attr('width', width)
+        .attr('height', height);
+
+      const g = svg
+        .append('g')
+        .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+      const pie = d3.pie().value((d) => d);
+
+      const data_ready = pie(data);
+
+      g.selectAll('path')
+        .data(data_ready)
+        .join('path')
+        .attr(
+          'd',
+          d3
+            .arc()
+            .innerRadius(100)
+            .outerRadius(120),
+        )
+        .attr('fill', (d) => color(d.data));
+    },
+  },
 };
 </script>
 
@@ -50,12 +96,7 @@ export default {
 }
 svg {
   position: absolute;
-  width: 15.2rem;
-  height: 15.2rem;
-  background-color: #ffc53457;
-  border-radius: 100%;
   z-index: -1;
-  top: 11.5%;
 }
 header {
   width: 15.2rem;
